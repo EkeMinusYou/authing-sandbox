ee58ae1c6b94d9def0cccbb2a1cfa3774664166e
EkeMinusYou <euonymuslke@gmail.com>
2023-01-12 18:00:41 +0900

    add secret

--- src/sign-in-by-username-password.ts
+++ src/sign-in-by-username-password.ts
@@ -2,9 +2,11 @@ import { AuthenticationClient } from "authing-node-sdk";
 
 const AUTHING_APP_ID = process.env.AUTHING_APP_ID ?? "";
 const AUTHING_APP_HOST = process.env.AUTHING_APP_HOST ?? "";
+const AUTHING_APP_SECRET = process.env.AUTHING_APP_SECRET ?? "";
 
 const authenticationClient = new AuthenticationClient({
   appId: AUTHING_APP_ID,
+  appSecret: AUTHING_APP_SECRET,
   appHost: AUTHING_APP_HOST,
 });
 
@@ -17,6 +19,7 @@ export async function signInByUsernamePassword(params: {
     password: params.password,
     options: { scope: "openid profile offline_access" },
   });
+  console.log(result.message);
   console.log(JSON.stringify(result.data));
   return;
 }
